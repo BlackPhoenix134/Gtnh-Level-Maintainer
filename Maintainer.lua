633bd1f900d5baa613afa65a3ec4f4fe394f85c3
BlackPhoenix134 <fabian.hermann@hotmail.com>
2026-01-11 15:46:49 +0100

    Revert "added heartbeat log"
    This reverts commit bd2173b2df3178bf3afaf5d79e816fa90dc98a1f.

--- Maintainer.lua
+++ Maintainer.lua
@@ -6,41 +6,28 @@ local util = require("src.Utility")
 
 local items = cfg.items
 local sleepInterval = cfg.sleep
-
-local function sleepWithHeartbeat(totalSeconds)
-  local HEARTBEAT = 60
-  local remaining = totalSeconds
-
-  while remaining > 0 do
-    local chunk = (remaining > HEARTBEAT) and HEARTBEAT or remaining
-    os.sleep(chunk)
-    remaining = remaining - chunk
-
-    if totalSeconds > HEARTBEAT and remaining > 0 then
-      logInfo("Running...")
-    end
-  end
-end
-
+ 
 local count = 0
 for _ in pairs(items) do
   count = count + 1
 end
 logInfo("Loaded " .. count .. " items to maintain.")
 
-logInfo("Running...")
-
 while true do
-  local itemsCrafting = ae2.checkIfCrafting()
+    local itemsCrafting = ae2.checkIfCrafting()
 
-  for item, config in pairs(items) do
-    if itemsCrafting[item] ~= true then
-      local success, answer = ae2.requestItem(item, config[1], config[2], config[3])
-      if success == false and answer ~= nil then
-        logInfo("Error: " .. answer)
-      end
+    for item, config in pairs(items) do
+        if itemsCrafting[item] == true then
+           -- logInfo(item .. " is already being crafted, skipping...")
+        else
+            local success, answer = ae2.requestItem(item, config[1], config[2], config[3])
+            if(success == true and answer ~= nil) then
+                logInfo(answer)
+            elseif(success == false and answer ~= nil) then
+                logInfo("Error: " .. answer)
+            end
+        end
+
     end
-  end
-
-  sleepWithHeartbeat(sleepInterval)
+    os.sleep(sleepInterval)
 end
