25433bd9a65227f5148b8522a42add8827a7cf92
BlackPhoenix134 <fabian.hermann@hotmail.com>
2026-01-11 15:28:18 +0100

    uncache config and log for items to maintain

--- Maintainer.lua
+++ Maintainer.lua
@@ -1,3 +1,5 @@
+package.loaded.config=nil
+
 local ae2 = require("src.AE2")
 local cfg = require("config")
 local util = require("src.Utility") 
@@ -5,9 +7,15 @@ local util = require("src.Utility")
 local items = cfg.items
 local sleepInterval = cfg.sleep
  
+local count = 0
+for _ in pairs(items) do
+  count = count + 1
+end
+logInfo("Loaded " .. count .. " items to maintain.")
+
 while true do
     local itemsCrafting = ae2.checkIfCrafting()
- 
+
     for item, config in pairs(items) do
         if itemsCrafting[item] == true then
            -- logInfo(item .. " is already being crafted, skipping...")
@@ -19,7 +27,7 @@ while true do
                 logInfo("Error: " .. answer)
             end
         end
- 
+
     end
     os.sleep(sleepInterval)
 end
